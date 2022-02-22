0ff6856d63d9ef1f6dc5622f970a558b9c7323aa
Renore49 <memovan0506@gmail.com>
2022-02-22 15:26:28 +0600

    hwjs1

---
+++ js/index.js
@@ -0,0 +1,66 @@
+// выражения
+// функция
+// объект
+
+// тип данных объект {}
+
+// const person = {
+// 	name: 'yryskeldi',
+// 	lastName: 'Dzhunusaliev',
+// 	age: 19,
+
+// 	sayHello: () => {
+// 		alert('Привет всем мое имя Ырыскелди')
+// 	},
+// }
+
+// // console.log(person.sayHello())
+// console.log('hello world...')
+// console.table(person)
+
+// переменные var, let, const
+
+// let ye = 2022
+// console.log(ye)
+// ye = 2023
+// console.log(ye)
+
+// let name
+// console.log(name)
+// name = 'ogogo'
+// console.log(name)
+let num = 3
+alert(num)
+
+let a = 10
+let b = 2
+console.log(a + b, a - b, a * b, a / b)
+let c = 15
+let d = 2
+let result = c + d
+
+alert(result)
+let str = 'Привет, Мир!'
+console.log(str)
+
+let str1 = 'Привет, '
+let str2 = 'Мир!'
+console.log(str1 + str2)
+
+let name = 'Yryskeldi'
+console.log(str1 + name)
+
+let youGuy = true
+let str3 = 'hello'
+let n1 = 123
+let bigInt = 121313n
+let hello = null
+
+let homeWork = {
+	youGuy,
+	str3,
+	n1,
+	bigInt,
+	hello,
+}
+console.log(homeWork)
